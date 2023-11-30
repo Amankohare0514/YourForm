9503dc46543cea729c533ccafee77f9fa7666485
Aman kohare <amankohare@gmail.com>
2023-11-30 18:31:35 +0530

    added

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ function Logo() {
       href={"/"}
       className="font-bold text-3xl bg-gradient-to-r from-red-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
     >
-      Reform
+    YourForm
     </Link>
   );
 }
